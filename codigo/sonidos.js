7a6a31be209bef7f4aa1412815e7a5de0a20d143
SIMOKLK <alexsimoesfernandez@gmail.com>
2020-11-20 14:28:35 +0100

    botonesCambioUnicoBG

--- codigo/sonidos.js
+++ codigo/sonidos.js
@@ -1,13 +1,20 @@
  document.addEventListener("click", function(event){
-            let instrumento = event.target.dataset.instrumento;
-            const sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
+
+    let instrumento = event.target.dataset.instrumento;
+    
+    let audioInstrumento = document.querySelector("audio[data-instrumento=" + instrumento + "]");
+
+    audioInstrumento.play();
+    audioInstrumento.currentTime = 0;
+    console.log(instrumento);
+
+
+    let animado = event.target.classList.add("cambio");
+    
+
+           
+    
+
+           
             
-            let bordes = document.getElementsByClassName("botones");
-            const animacion = document.querySelectorAll("button[data-instrumento"+ bordes + "]")
-            bordes.style.border = (bordes.style.border === "none") ? "block" : "none";
-            if(!sonido) return;
-            sonido.currentTime = 0;
-            sonido.play();
-            animacion.on();
-            console.log(instrumento);
         });
