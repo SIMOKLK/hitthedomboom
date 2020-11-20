05c7ed22177ab65ba707fdb0d3cd9d4a153e547e
PPIICCAA <AbdulhaIdrees@gmail.com>
2020-11-20 11:24:37 +0100

    Version yo k se

--- codigo/sonidos.js
+++ codigo/sonidos.js
@@ -1,8 +1,13 @@
  document.addEventListener("click", function(event){
             let instrumento = event.target.dataset.instrumento;
             const sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
+            
+            let bordes = document.getElementsByClassName("botones");
+            const animacion = document.querySelectorAll("button[data-instrumento"+ bordes + "]")
+            bordes.style.border = (bordes.style.border === "none") ? "block" : "none";
             if(!sonido) return;
             sonido.currentTime = 0;
             sonido.play();
+            animacion.on();
             console.log(instrumento);
         });
